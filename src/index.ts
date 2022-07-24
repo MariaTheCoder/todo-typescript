b0c1b2ab56fb4f9f5778c76274c954dbb13ce4e8
Maria T. Andersen <mteilmann93@gmail.com>
2022-07-24 16:48:03 +0200

    Get the unordered list, form and input elements by id and save the HTML elements in constant variables. Make use of angle brackets to specify which HTML elements these are expected be

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 import { v4 as uuidV4 } from 'uuid';
-console.log(uuidV4());
+
+const list = document.querySelector<HTMLUListElement>('#list');
+const form = document.querySelector<HTMLFormElement>('#new-task-form');
+const input = document.querySelector<HTMLInputElement>('#new-task-title');
