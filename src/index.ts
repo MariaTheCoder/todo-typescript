13fb29b586586b38e2405d41c113697e0c6af541
Maria T. Andersen <mteilmann93@gmail.com>
2022-07-24 16:52:55 +0200

    Add an event listnener to the form element and add an eventhandler that, assuming that input exists, and its value if different from "" or null, will create a new Task. Ensure that TypeScript will find types for uuid library. If not, install the types for the library

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,14 @@ import { v4 as uuidV4 } from 'uuid';
 const list = document.querySelector<HTMLUListElement>('#list');
 const form = document.querySelector<HTMLFormElement>('#new-task-form');
 const input = document.querySelector<HTMLInputElement>('#new-task-title');
+
+form?.addEventListener('submit', (e) => {
+  if (input?.value == '' || input?.value == null) return;
+
+  const newTask = {
+    id: uuidV4(),
+    title: input?.value,
+    completed: false,
+    createdAt: new Date(),
+  };
+});
